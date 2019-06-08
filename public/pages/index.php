66a785128dc636a786b09afb5ab5d65bf45dd242
Danil Kozyr <danilkozyr@MacBook-Pro-Danil.local>
2019-06-09 01:42:33 +0300

    new home page

--- public/pages/index.php
+++ public/pages/index.php
@@ -3,4 +3,23 @@
 <?php include(SHARED_PATH . '/header.php'); ?>
 
 
+<table class="home-page">
+	<tr>
+		<th>
+			<div class="home-heding-div">
+				<p class="home-heading1">42 school project</p>
+				<p class="home-heading2">Camagru</p>
+				<p class="home-heading3">Daniil Kozyr</p>
+				<p class="home-heading4">dkozyr@student.unit.ua</p>
+
+
+			</div>
+		</th>
+		<th>
+			<img src="../images/home_banner.png">
+		</th>
+	</tr>
+</table>
+
+
 <?php include(SHARED_PATH . '/footer.php'); ?>
